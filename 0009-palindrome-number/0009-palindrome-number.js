9989afb6472cfd397983ba68ac73da60615cc654
Misna_pp <miisnaaah@gmail.com>
2026-07-27 09:31:49 +0530

    Time: 106 ms (5.06%), Space: 63.6 MB (63.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,14 @@
+function isPalindrome(num){
+
+    let original = num;
+    let rev = 0;
+    while(num>0){
+            let digit = num % 10;
+            rev =rev*10+digit;
+            num =  Math.floor(num / 10);
+        }
+
+        return original == rev;
+}
+
+console.log(isPalindrome(121));

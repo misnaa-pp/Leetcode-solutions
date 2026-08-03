eb10344ac91c6dfffbf73b52ecbc1474dec0bbf9
Misna_pp <miisnaaah@gmail.com>
2026-08-03 13:33:20 +0530

    Time: 1 ms (89.83%), Space: 54.8 MB (67.8%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,21 @@
+function countEven(num){
+
+    let count = 0;
+    for(let i=1; i<=num; i++){
+
+        let n = i;
+        let sum = 0;
+
+        while(n>0){
+            let digit = n % 10;
+            sum+=digit;
+            n = Math.floor(n / 10);
+        }
+        if(sum % 2 === 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countEven(4));

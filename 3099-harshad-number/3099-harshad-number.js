19d9450d26c3cd50be0df007a86f1c656919e6db
Misna_pp <miisnaaah@gmail.com>
2026-08-05 16:14:44 +0530

    Time: 0 ms (100%), Space: 51.5 MB (100%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.js
@@ -0,0 +1,15 @@
+function sumOfTheDigitsOfHarshadNumber(x){
+    
+    let sum = 0;
+
+    for(let num of String(x)){
+        sum+=Number(num);
+    }
+    if(x % sum === 0){
+            return sum;
+        }
+
+    return -1;
+}
+
+console.log(sumOfTheDigitsOfHarshadNumber(18));

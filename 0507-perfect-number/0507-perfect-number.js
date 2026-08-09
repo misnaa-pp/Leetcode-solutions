82e939a84df9ede2a97189acb14f40905c145b26
Misna_pp <miisnaaah@gmail.com>
2026-08-09 11:59:00 +0530

    Time: 2125 ms (5.3%), Space: 55.1 MB (48.91%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,17 @@
+function checkPerfectNumber(n){
+
+    let sum = 0;
+
+    for (let i=1; i<n; i++){
+        if(n % i === 0){
+            sum+=i;
+        }
+    }
+    if(sum === n){
+        return true;
+    }
+        return false;
+    
+}
+
+console.log(checkPerfectNumber(28));

46c221f8619cc2247c9b5aa7ef2e121c4177e9c7
Misna_pp <miisnaaah@gmail.com>
2026-08-14 09:35:30 +0530

    Time: 1 ms (44.93%), Space: 54.1 MB (26.81%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -7,4 +7,4 @@ var truncateSentence = function(s, k) {
     return s.split(" ").slice(0, k).join(" ");
 };
 
-console.log(truncateSentence("Hello how are you Contestant",4));
+console.log(truncateSentence("What is the solution to this problem",4));

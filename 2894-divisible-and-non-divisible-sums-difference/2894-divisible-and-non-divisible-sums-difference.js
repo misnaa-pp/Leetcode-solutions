50e2da394a496e0b1693cb7677f3ee9b247b7744
Misna_pp <miisnaaah@gmail.com>
2026-08-19 16:28:20 +0530

    Time: 0 ms (100%), Space: 55.1 MB (76.73%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,20 @@
+
+var differenceOfSums = function(n, m) {
+    let sum1 =0;
+    let sum2 = 0;
+    for (let i=1;i<=n;i++){
+        if(i % m != 0){
+            sum1+=i;
+        }
+    }
+    for (let j=1;j<=n;j++){
+        if(j % m === 0){
+            sum2+=j;
+        }
+    }
+
+    return sum1-sum2;
+    
+};
+
+console.log(differenceOfSums(10,3))
